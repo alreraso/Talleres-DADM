c271f1f7f9b46e20c8fc2348b6e81bacbf846aeb
Rene <alrramirezso@unal.edu.co>
2021-12-06 20:26:40 -0500

    firebase conectado lee y guarda datos :penguin:

---
+++ Reto6/app/src/main/java/co/edu/unal/androidtic_tac_toe/Rooms.java
@@ -0,0 +1,14 @@
+package co.edu.unal.androidtic_tac_toe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Rooms extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rooms);
+    }
+}
